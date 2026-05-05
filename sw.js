3fd8fbab70f6933e447608bf2fc44ab7bf8fa136
zafarluni <zafarluni@yahoo.com>
2026-05-05 12:15:30 +0000

    Update from https://github.com/zafarluni/packman/commit/610fb87f82ac1f848b45ffd8597e665d0c126a89

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'legacy-pacman-v7';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[SW] Caching all assets');
+                return cache.addAll([
+                    '/pcm/',
+                    '/pcm/index.html',
+                    '/pcm/manifest.json'
+                ]);
+            })
+            .then(() => {
+                console.log('[SW] Assets cached successfully');
+                return self.skipWaiting();
+            })
+            .catch((err) => {
+                console.error('[SW] Failed to cache assets:', err);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames
+                        .filter((name) => name !== CACHE_NAME)
+                        .map((name) => caches.delete(name))
+                );
+            })
+            .then(() => {
+                console.log('[SW] Old caches cleared');
+                return self.clients.claim();
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+
+    if (url.origin === location.origin) {
+        event.respondWith(
+            caches.match(event.request)
+                .then((cachedResponse) => {
+                    if (cachedResponse) {
+                        return cachedResponse;
+                    }
+                    return fetch(event.request)
+                        .then((networkResponse) => {
+                            if (networkResponse && networkResponse.status === 200) {
+                                const responseClone = networkResponse.clone();
+                                caches.open(CACHE_NAME)
+                                    .then((cache) => {
+                                        cache.put(event.request, responseClone);
+                                    });
+                            }
+                            return networkResponse;
+                        });
+                })
+                .catch(() => {
+                    if (event.request.destination === 'document') {
+                        return caches.match('/pcm/index.html');
+                    }
+                })
+        );
+    }
+});
